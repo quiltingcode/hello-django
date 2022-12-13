a7e1d686caac910854d51feac309a88085e67c20
Kelly Hutchison <kellyhutchison@hotmail.com>
2022-12-13 11:56:22 +0000

    Create models tests

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -3,7 +3,7 @@ from .forms import ItemForm
 
 
 class TestItemForm(TestCase):
-    
+
     def test_item_name_is_required(self):
         form = ItemForm({'name': ''})
         self.assertFalse(form.is_valid())
@@ -17,4 +17,3 @@ class TestItemForm(TestCase):
     def test_fields_are_explicit_in_form_metaclass(self):
         form = ItemForm()
         self.assertEqual(form.Meta.fields, ['name', 'done'])
-        
--- todo/test_views.py
+++ todo/test_views.py
@@ -37,8 +37,3 @@ class TestViews(TestCase):
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
         self.assertFalse(updated_item.done)
-
-
-
-        
-
