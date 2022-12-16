f1c6f222f3413cb5151adb00b1803eb66010c9a0
Kelly Hutchison <kellyhutchison@hotmail.com>
2022-12-16 10:29:11 +0000

    Change development to true

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 if os.path.isfile("env.py"):
     import env
 
-development = os.environ.get('DEVELOPMENT', False)
+development = os.environ.get('DEVELOPMENT', True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
